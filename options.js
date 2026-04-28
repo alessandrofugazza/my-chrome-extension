93e5f235357c394b29ffc9b286056032bc21381d
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-04-28 05:55:08 +0200

    trying to change popup from options

---
+++ options.js
@@ -0,0 +1,14 @@
+// const productionModeBtn = document.getElementById("production-mode-btn");
+// const pgModeBtn = document.getElementById("pg-mode-btn");
+
+// productionModeBtn.addEventListener("click", () => {
+//   chrome.action.setPopup({ popup: "popup.html" }, () => {
+//     console.log("Popup set to production mode");
+//   });
+// });
+
+// pgModeBtn.addEventListener("click", () => {
+//   chrome.action.setPopup({ popup: "popup-pg.html" }, () => {
+//     console.log("Popup set to pg mode");
+//   });
+// });
--- other-popup.js
+++
@@ -1,5 +0,0 @@
-const goToPopupBtn = document.getElementById("go-to-popup-btn");
-
-goToPopupBtn.addEventListener("click", () => {
-  chrome.action.setPopup({ popup: "popup.html" });
-});

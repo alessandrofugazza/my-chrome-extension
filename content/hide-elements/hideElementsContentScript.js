babbbbb512bd7f9f8415519957a99ce316d3b4a3
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-05-27 06:06:30 +0200

    update hidden-profile text

--- content/hide-elements/hideElementsContentScript.js
+++ content/hide-elements/hideElementsContentScript.js
@@ -1,5 +1,43 @@
 console.log("Secret content script loaded");
 
+function updateHiddenProfileText() {
+  const emptyFeedText = document.querySelector("#empty-feed-content .text-body-1");
+
+  if (!emptyFeedText) return;
+
+  const originalText = emptyFeedText.textContent.trim();
+
+  const match = originalText.match(
+    /^(u\/[^\s]+)\s+likes to keep their posts hidden, but check out their stats to learn more about them./,
+  );
+
+  if (!match) return;
+
+  const username = match[1];
+  const cleanUsername = username.replace("u/", "");
+
+  const redditSearchUrl = `https://www.google.com/search?q=` + encodeURIComponent(`site:reddit.com ${username}`);
+
+  emptyFeedText.innerHTML = `
+    ${username} can suck a dick, but you can <br>
+    <br>
+    <a
+      href="${redditSearchUrl}"
+      target="_blank"
+      style="color:#4f8cff;"
+    >
+      Search user on Reddit
+    </a>
+  `;
+}
+
+updateHiddenProfileText();
+
+new MutationObserver(updateHiddenProfileText).observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
 // function styleSubtitles() {
 //   document.querySelectorAll(".vjs-text-track-cue").forEach((cue) => {
 //     const parent = cue.parentElement;

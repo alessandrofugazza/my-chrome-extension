5ad52e856700372e87d333a78c7ff644bcbe310e
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-05-27 19:50:29 +0200

    before splitting hideelements per site

--- content/hide-elements/hideElementsContentScript.js
+++ content/hide-elements/hideElementsContentScript.js
@@ -38,27 +38,27 @@ new MutationObserver(updateHiddenProfileText).observe(document.body, {
   subtree: true,
 });
 
-// function styleSubtitles() {
-//   document.querySelectorAll(".vjs-text-track-cue").forEach((cue) => {
-//     const parent = cue.parentElement;
+function styleSubtitles() {
+  document.querySelectorAll(".vjs-text-track-cue").forEach((cue) => {
+    const parent = cue.parentElement;
 
-//     if (parent) {
-//       parent.style.fontSize = "1rem";
-//       // parent.style.lineHeight = "1.4";
-//     }
+    if (parent) {
+      parent.style.setProperty("font-size", "1.5rem", "important");
+      // parent.style.lineHeight = "1.4";
+    }
 
-//     cue.style.fontSize = "1rem";
-//     // cue.style.lineHeight = "1.4";
-//   });
-// }
+    cue.style.setProperty("font-size", "1.5rem", "important");
+    // cue.style.lineHeight = "1.4";
+  });
+}
 
-// styleSubtitles();
+styleSubtitles();
 
-// const observer = new MutationObserver(() => {
-//   styleSubtitles();
-// });
+const observer = new MutationObserver(() => {
+  styleSubtitles();
+});
 
-// observer.observe(document.body, {
-//   childList: true,
-//   subtree: true,
-// });
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});

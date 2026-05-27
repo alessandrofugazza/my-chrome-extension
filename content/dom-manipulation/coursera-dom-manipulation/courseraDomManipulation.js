0d37cc80e39fa669f89c35473377083a91fa5593
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-05-27 20:19:43 +0200

    split dom manipulation files per website

---
+++ content/dom-manipulation/coursera-dom-manipulation/courseraDomManipulation.js
@@ -0,0 +1,24 @@
+function styleSubtitles() {
+  document.querySelectorAll(".vjs-text-track-cue").forEach((cue) => {
+    const parent = cue.parentElement;
+
+    if (parent) {
+      parent.style.setProperty("font-size", "1.5rem", "important");
+      // parent.style.lineHeight = "1.4";
+    }
+
+    cue.style.setProperty("font-size", "1.5rem", "important");
+    // cue.style.lineHeight = "1.4";
+  });
+}
+
+styleSubtitles();
+
+const observer = new MutationObserver(() => {
+  styleSubtitles();
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
--- content/dom-manipulation/reddit-dom-manipulation/redditDomManipulation.js
+++ content/dom-manipulation/reddit-dom-manipulation/redditDomManipulation.js
@@ -1,5 +1,3 @@
-console.log("Secret content script loaded");
-
 function updateHiddenProfileText() {
   const emptyFeedText = document.querySelector("#empty-feed-content .text-body-1");
 
@@ -34,31 +32,6 @@ function updateHiddenProfileText() {
 updateHiddenProfileText();
 
 new MutationObserver(updateHiddenProfileText).observe(document.body, {
-  childList: true,
-  subtree: true,
-});
-
-function styleSubtitles() {
-  document.querySelectorAll(".vjs-text-track-cue").forEach((cue) => {
-    const parent = cue.parentElement;
-
-    if (parent) {
-      parent.style.setProperty("font-size", "1.5rem", "important");
-      // parent.style.lineHeight = "1.4";
-    }
-
-    cue.style.setProperty("font-size", "1.5rem", "important");
-    // cue.style.lineHeight = "1.4";
-  });
-}
-
-styleSubtitles();
-
-const observer = new MutationObserver(() => {
-  styleSubtitles();
-});
-
-observer.observe(document.body, {
   childList: true,
   subtree: true,
 });
